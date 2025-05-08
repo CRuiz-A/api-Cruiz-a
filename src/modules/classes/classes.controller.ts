6eb6e192f154bdd743ec57f9a860e70ba270c9e5
CRuiz-A <2cristianruiz5@gmail.com>
2025-05-07 21:44:05 -0500

    v2

--- src/modules/classes/classes.controller.ts
+++ src/modules/classes/classes.controller.ts
@@ -5,9 +5,15 @@ import { ClassesService } from './classes.service';
 export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
-  @Get('by-date')
+  @Get
+  ('by-date')
   async findClassesByDate(@Query('date') date: string) {
     // Assuming 'America/Bogota' timezone for now, this could be made dynamic
     return this.classesService.getClassesByDate(date, 'America/Bogota');
+  }
+
+  @Get('by-student-email')
+  async findClassesByStudentEmail(@Query('email') email: string) {
+    return this.classesService.getClassesByStudentEmail(email);
   }
 }
